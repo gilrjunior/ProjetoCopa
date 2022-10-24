85124d86c62b54aee7c07db5f0c226305bb87c69
gilrjunior <gilmarjunioreis@gmail.com>
2022-10-24 16:45:28 -0300

    Third commit

--- src/principal/Principal.java
+++ src/principal/Principal.java
@@ -1,28 +1,39 @@
 package principal;
 
 
+import java.util.ArrayList;
 import javax.swing.JOptionPane;
-//import selecao.Selecao;
+import selecao.Selecao;
 
 public class Principal {
 
     public static void main(String[] args) {
         
-        while(true){
+        ArrayList <Selecao> listajogadores = new ArrayList();
+        
+        int opc = 0;
+        String Nome, posicao;
+        int numero;
+        boolean titular;
             
-            String Nome, posicao;
-            int numero;
+       do{ 
+           opc = Integer.parseInt(JOptionPane.showInputDialog("1- Inserir jogador 2- Sair e mostrar"));
+
             
             Nome = JOptionPane.showInputDialog("Insira o nome do jogador: ");
             posicao = JOptionPane.showInputDialog("Insira a posicao de "+Nome+": ");
-            numero = Integer.parseInt(JOptionPane.showInputDialog("Insira o nome: "));
+            numero = Integer.parseInt(JOptionPane.showInputDialog("Insira o numero de "+Nome+": "));
+            titular = Boolean.getBoolean(JOptionPane.showInputDialog(Nome+" é titula? True or False")); 
             
             
+            listajogadores.add(new Selecao(Nome, posicao, numero, titular));
             
-            
-        }
+        }while(opc != 2);
         
-        
+
+         JOptionPane.showMessageDialog(null, listajogadores);
+
+                
     }
     
 }
